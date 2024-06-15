64d39d673ddf81463c642617acead1b4c49c2439
Pixoll <pixoll.gamer@gmail.com>
2024-06-15 16:45:31 -0400

    Works but compressed size is not desirable
    Trie no longer used

--- lempel_ziv.hpp
+++ lempel_ziv.hpp
@@ -1,35 +1,44 @@
 #pragma once
 
-#include "trie.hpp"
-
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
+
+typedef unsigned long long uint64;
 
 class lempel_ziv {
 public:
     typedef vector<pair<uint64, uint64>> compressed_t;
 
-    // TODO: fails on repeated characters
+    // TODO: compressed size is not quite there yet
     static compressed_t compress(const string &input) {
-        trie trie;
+        unordered_map<char, uint64> chars;
         compressed_t compressed;
 
-        for (uint64 i = 0; i < input.size();) {
-            auto [position, length] = trie.search(input, i);
-
-            if (length == 0) {
-                compressed.emplace_back(input[i], 0);
-                trie.insert(input, i, 1);
+        for (uint64 i = 0; i < input.length();) {
+            const char c1 = input[i];
+            if (chars.count(c1) == 0) {
+                chars[c1] = i;
+                compressed.emplace_back(c1, 0);
                 i++;
                 continue;
             }
 
+            const uint64 position = chars[c1];
+            uint64 length = 1;
+            for (uint64 j = 1; j < input.length() - i; j++) {
+                const char c2 = input[i + j];
+                if (chars.count(c2) == 0 || chars[c2] != position + j)
+                    break;
+
+                length++;
+            }
+
             compressed.emplace_back(position, length);
-            trie.insert(input, i, length + 1);
             i += length;
         }
 

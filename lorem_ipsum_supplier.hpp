74ccb13c76df0b5f3dce731dc9615b32df01e5be
Pixoll <pixoll.gamer@gmail.com>
2024-06-15 21:55:41 -0400

    Allow testing on multiple datasets

---
+++ lorem_ipsum_supplier.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+class lorem_ipsum_supplier {
+private:
+    string lorem_ipsum;
+    int index = 0;
+
+public:
+    lorem_ipsum_supplier() {
+        ifstream file("lorem_ipsum.txt");
+        if (!file.is_open()) {
+            cerr << "could not open lorem_ipsum.txt, cannot proceed" << endl;
+            exit(1);
+        }
+
+        stringstream buffer;
+        buffer << file.rdbuf();
+        lorem_ipsum = buffer.str();
+    }
+
+    ~lorem_ipsum_supplier() = default;
+
+    char operator()() {
+        return this->lorem_ipsum[this->index++];
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include "huffman.hpp"
 #include "lempel_ziv.hpp"
+#include "lorem_ipsum_supplier.hpp"
 #include "performance.hpp"
 #include "random_char_gen.hpp"
 
 #include <iostream>
 #include <filesystem>
+#include <functional>
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <utility>
 
 using namespace std;
 
@@ -15,61 +18,68 @@ int main() {
     if (!filesystem::exists("data"))
         filesystem::create_directory("data");
 
-    random_char_gen random_char(1);
-    stringstream out_hm, out_lz;
-    string input;
-    performance p;
-
     huffman warmup;
     warmup.decode(warmup.encode("warmup"));
     warmup.~huffman();
 
     lempel_ziv::decompress(lempel_ziv::compress("warmup"));
 
-    out_hm << "length,encode_t,decode_t,bits\n";
-    out_lz << "length,compress_t,decompress_t,bits\n";
+    pair<string, function<char()>> test_params[] = {
+            {"randomized",  random_char_gen(1)},
+            {"lorem_ipsum", lorem_ipsum_supplier()},
+    };
 
-    for (int length = 1; length <= 1000; length++) {
-        input += random_char();
+    for (const auto &[name, generator]: test_params) {
+        stringstream out_hm, out_lz;
+        string input;
+        performance p;
 
-        for (int _ = 0; _ < 50; ++_) {
-            huffman h;
+        out_hm << "length,encode_t,decode_t,bits\n";
+        out_lz << "length,compress_t,decompress_t,bits\n";
 
-            p.start();
-            const string &encoded = h.encode(input);
-            const uint64 encode_time = p.end();
+        for (int length = 1; length <= 1000; length++) {
+            input += generator();
 
-            p.start();
-            h.decode(encoded);
-            const uint64 decode_time = p.end();
+            for (int _ = 0; _ < 50; ++_) {
+                huffman h;
 
-            p.start();
-            const lempel_ziv::compressed_t &compressed = lempel_ziv::compress(input);
-            const uint64 compress_time = p.end();
+                p.start();
+                const string &encoded = h.encode(input);
+                const uint64 encode_time = p.end();
 
-            p.start();
-            lempel_ziv::decompress(compressed);
-            const uint64 decompress_time = p.end();
+                p.start();
+                h.decode(encoded);
+                const uint64 decode_time = p.end();
 
-            out_hm << length << ","
-                   << encode_time << ","
-                   << decode_time << ","
-                   << encoded.length() << "\n";
+                p.start();
+                const lempel_ziv::compressed_t &compressed = lempel_ziv::compress(input);
+                const uint64 compress_time = p.end();
 
-            out_lz << length << ","
-                   << compress_time << ","
-                   << decompress_time << ","
-                   << compressed.size() * 8 << "\n";
+                p.start();
+                lempel_ziv::decompress(compressed);
+                const uint64 decompress_time = p.end();
+
+                out_hm << length << ","
+                       << encode_time << ","
+                       << decode_time << ","
+                       << encoded.length() << "\n";
+
+                out_lz << length << ","
+                       << compress_time << ","
+                       << decompress_time << ","
+                       << compressed.size() * 8 << "\n";
+            }
         }
+
+        ofstream file_hm("data/huffman_" + name + ".csv");
+        ofstream file_lz("data/lempel_ziv_" + name + ".csv");
+
+        file_hm << out_hm.rdbuf();
+        file_lz << out_lz.rdbuf();
+
+        file_hm.close();
+        file_lz.close();
     }
-
-    ofstream file_hm("data/huffman.csv"), file_lz("data/lempel_ziv.csv");
-
-    file_hm << out_hm.rdbuf();
-    file_lz << out_lz.rdbuf();
-
-    file_hm.close();
-    file_lz.close();
 
     return 0;
 }

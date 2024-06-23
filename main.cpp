3e69a16cf23a474515700b1ea095f77990e87d02
Pixoll <pixoll.gamer@gmail.com>
2024-06-23 15:48:32 -0400

    Multithreading baby

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "tests.hpp"
 
 #include <filesystem>
+#include <thread>
 
 using namespace std;
 
@@ -20,12 +21,24 @@ int main(const int argc, const char *argv[]) {
     huffman_greedy::decode(huffman_greedy::encode(test_string));
     lempel_ziv_gfg::decompress(lempel_ziv_gfg::compress(test_string));
 
-    const int tests = argc > 1 ? strtol(argv[1], nullptr, 10) : 1;
+    const int tests = argc > 1 ? strtol(argv[1], nullptr, 10) : 100;
     const int iterations = argc > 2 ? strtol(argv[2], nullptr, 10) : 20;
 
-    run_tests("lorem-ipsum", "lorem_ipsum.txt", tests, iterations);
-    run_tests("duplicate", "duplicate.txt", tests, iterations);
-    run_tests("butterfly", "butterfly.jpg", tests, iterations);
+    thread lorem_ipsum_thread([](int tests, int iterations) {
+        run_tests("lorem-ipsum", "lorem_ipsum.txt", tests, iterations);
+    }, tests, iterations);
+
+    thread duplicate_thread([](int tests, int iterations) {
+        run_tests("duplicate", "duplicate.txt", tests, iterations);
+    }, tests, iterations);
+
+    thread butterfly_thread([](int tests, int iterations) {
+        run_tests("butterfly", "butterfly.jpg", tests, iterations);
+    }, tests, iterations);
+
+    lorem_ipsum_thread.join();
+    duplicate_thread.join();
+    butterfly_thread.join();
 
     return 0;
 }
